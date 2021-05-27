e9d777eb2b7c4cd81f3d80b13b575e2efacb5ce6
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-05-27 12:35:06 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
+
+<div class="flex">
+	<div class="text-blue-500 font-bold">TEST</div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./src/**/*.{html,js,svelte,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
