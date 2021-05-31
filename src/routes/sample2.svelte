d1a73dd6078e646d70e43a04d4b21bbd631cc2d7
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-05-31 13:33:08 +0530

    centered sample 2

--- src/routes/sample2.svelte
+++ src/routes/sample2.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
 <svelte:head>
 	<title>Sample 2</title>
 </svelte:head>
 
-<h1>About this site</h1>
-<p>TODO...</p>
+<div transition:fade class="flex justify-center align-middle items-center p-2 m-2">
+	<h1>About this site</h1>
+	<p>TODO...</p>
+</div>
