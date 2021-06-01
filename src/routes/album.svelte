9535d8744ca3df642458c114b60392abf020ea8b
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-06-01 17:12:12 +0530

    removed head

--- src/routes/album.svelte
+++ src/routes/album.svelte
@@ -6,10 +6,6 @@
 	import PhotoAlbum from '$lib/PhotoAlbum.svelte';
 </script>
 
-<svelte:head>
-	<title>World Bank</title>
-</svelte:head>
-
 <div class="flex justify-center align-middle items-center">
 	<div class="p-4 m-2 border border-gray-200 rounded shadow">
 		<PhotoAlbum />
