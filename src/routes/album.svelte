6246ed50a2cacbbc733cde4cded99adf924e7168
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-06-01 17:14:59 +0530

    added favicon

--- src/routes/album.svelte
+++ src/routes/album.svelte
@@ -6,6 +6,10 @@
 	import PhotoAlbum from '$lib/PhotoAlbum.svelte';
 </script>
 
+<svelte:head>
+	<title>Photos</title>
+</svelte:head>
+
 <div class="flex justify-center align-middle items-center">
 	<div class="p-4 m-2 border border-gray-200 rounded shadow">
 		<PhotoAlbum />
