1df0b41e6cf870432b1ae3a180c53739ead9b8b6
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-06-03 17:02:29 +0530

    moving to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +15,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
+		adapter: adapter()
 	}
 };
 
