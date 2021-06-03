457b74d891ccc2f1cd4f004c6fc7ad7853a6c9ee
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-06-03 13:39:44 +0530

    removed the fn

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,36 @@
 	import { page } from '$app/stores';
 	import Tooltip from '$lib/Tooltip.svelte';
 	import { tooltip } from '../actions/tooltip';
-	import { initFirebase } from '../auth';
+	import {
+		FIREBASE_API_KEY,
+		FIREBASE_APP_ID,
+		FIREBASE_AUTH_DOMAIN,
+		FIREBASE_MESSAGING_SENDER_ID,
+		FIREBASE_PROJECT_ID,
+		FIREBASE_STORAGE_BUCKET
+	} from '$lib/Env';
+
+	const initFirebase = async () => {
+		const module = await import('firebase/app');
+		const firebase = module.default;
+		await Promise.all([import('firebase/auth')]);
+
+		const config = {
+			apiKey: FIREBASE_API_KEY,
+			authDomain: FIREBASE_AUTH_DOMAIN,
+			projectId: FIREBASE_PROJECT_ID,
+			storageBucket: FIREBASE_STORAGE_BUCKET,
+			messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+			appId: FIREBASE_APP_ID
+		};
+
+		if (!firebase.apps.length) {
+			firebase.initializeApp(config);
+		}
+
+		return firebase;
+	};
+
 	let showProfile = false;
 	let user = false;
 	const routes = [
