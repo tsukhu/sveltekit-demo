d49e9c880b051258f238caa24d25b37337fe8a60
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-06-09 13:20:50 +0530

    pre-render

--- src/lib/PhotoAlbum.svelte
+++ src/lib/PhotoAlbum.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script>
 	import { onMount } from 'svelte';
 
--- src/routes/album.svelte
+++ src/routes/album.svelte
@@ -1,3 +1,6 @@
+<script context="module">
+	export const prerender = true;
+</script>
 <script type="ts">
 	import PhotoAlbum from '$lib/PhotoAlbum.svelte';
 </script>
