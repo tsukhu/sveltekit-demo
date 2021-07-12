b759c83a4050bfcaf6c05c704e787e92489760c6
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-07-12 11:27:09 +0530

    updated versions

--- src/__tests__/Modal.test.ts
+++ src/__tests__/Modal.test.ts
@@ -2,7 +2,7 @@ import { render } from '@testing-library/svelte';
 import Modal from '../routes/Modal.svelte';
 
 test('should render', () => {
-	const results = render(Modal);
+	const { getByText } = render(Modal);
 
-	expect(() => results.getByText('Modal Dialog')).not.toThrow();
+	expect(() => getByText("Modal Dialog" as any)).not.toThrow();
 });
