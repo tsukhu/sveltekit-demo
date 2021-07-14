339624aa7218b1bf46fa7ab5d2b5cf1fcb40b348
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-07-14 17:33:43 +0530

    stencilSSR update

--- src/routes/api/stensvelteSSR.ts
+++ src/routes/api/stensvelteSSR.ts
@@ -2,7 +2,7 @@ import type { EndpointOutput } from '@sveltejs/kit';
 // Due to this issue given below using node_modules to import the
 // StencilJS Generated SSR hydrate function.
 // as an example pulished the hydrate function as a module
-//https://github.com/vitejs/vite/issues/2579
+// https://github.com/vitejs/vite/issues/2579
 import { renderToString, HydrateResults } from 'stensvelte_hydrate';
 export const get = async (): Promise<EndpointOutput> => {
 	const { html } = (await renderToString(
