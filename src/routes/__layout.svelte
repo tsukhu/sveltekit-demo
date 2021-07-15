4ab6ea2a550870c02f5bd656b023affadadb8d44
Tarun Kumar Sukhu <tksukhu@gmail.com>
2021-07-15 14:10:22 +0530

    updated anchor details

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -245,7 +245,7 @@
 				class="p-2 bg-white dark:bg-gray-900 flex text-gray-600 dark:text-gray-100 text-xs align-middle items-center justify-between shadow-sm border-t border-gray-100 dark:border-gray-700"
 			>
 				<div>tsukhu@github</div>
-				<div><a href="https://github.com/tsukhu/sveltekit-demo" target="_blank" aria-label="Github Repo" class="hover:text-green-700 dark:hover:text-green-200">Github Repo</a></div>
+				<div><a href="https://github.com/tsukhu/sveltekit-demo" target="_blank" rel="noreferrer" aria-label="Github Repo" class="hover:text-green-700 dark:hover:text-green-200">Github Repo</a></div>
 				<div>Powered by SvelteKit</div>
 			</footer>
 		</div>
