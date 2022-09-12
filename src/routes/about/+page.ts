f949b28a2ef7ab46e230c6fc498df92e582feee2
Tarun Kumar Sukhu <tksukhu@gmail.com>
2022-09-12 11:26:19 +0530

    updated hydrate to csr

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -1,9 +1,8 @@
 import { browser, dev } from '$app/environment';
-import SEO from '$lib/SEO.svelte';
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement...
-export const hydrate = dev;
+export const csr = dev;
 
 // ...but if the client-side router is already loaded
 // (i.e. we came here from elsewhere in the app), use it
